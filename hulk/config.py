d92650567d6be71b1fca841a3ff83c07fb0de28d
Lorne <dev@luotao.net>
2019-10-06 21:55:16 +1100

    add DEBUG

--- hulk/config.py
+++ hulk/config.py
@@ -3,6 +3,8 @@ from environs import Env
 env = Env()
 env.read_env()
 
+DEBUG = env.bool('DEBUG', default=False)
+
 OANDA_DOMAIN = env.str('OANDA_DOMAIN', 'DEMO')
 OANDA_ACCESS_TOKEN = env.str('OANDA_ACCESS_TOKEN', '')
 OANDA_ACCOUNT_ID = env.str('OANDA_ACCOUNT_ID', '')
