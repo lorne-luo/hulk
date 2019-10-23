1c00d84c0028252e78a7bebfa4c487abb1ce537e
Lorne <dev@luotao.net>
2019-10-23 16:53:03 +1100

    test:passed test for fxcm

--- tests/test_fxcm.py
+++ tests/test_fxcm.py
@@ -1,4 +1,5 @@
 import unittest
+from time import sleep
 
 from hulk.base import AccountType, OrderSide
 from hulk.broker.fxcm.account import FXCM
@@ -11,8 +12,11 @@ class FXCMTestAccount(unittest.TestCase):
 
     def setUp(self):
         self.account = FXCM(type=AccountType.DEMO,
-                                     account_id=test_config.FXCM_ACCOUNT_ID,
-                                     access_token=test_config.FXCM_ACCESS_TOKEN)
+                            account_id=test_config.FXCM_ACCOUNT_ID,
+                            access_token=test_config.FXCM_ACCESS_TOKEN)
+
+    def tearDown(self):
+        self.account.disconnect()
 
     def test_instrument(self):
         pass
@@ -25,20 +29,19 @@ class FXCMTestAccount(unittest.TestCase):
         self.assertTrue(order)
         self.assertEqual(len(self.account.open_positions), 1 + position_count)
         self.assertEqual(len(self.account.list_open_trade()), 1 + trade_count)
-
-        self.assertTrue(len(self.account.list_all_positions()))
         self.assertTrue(len(self.account.list_all_positions()))
 
-        trade_id = self.account.list_open_trade()[0]
+        trade_id = next(iter(self.account.list_open_trade()))
         trade = self.account.get_trade(trade_id)
         self.assertTrue(trade.get_amount())
-        self.account.take_profit(self, trade_id, 40, is_in_pips=True)
-        self.account.stop_loss(self, trade_id, -40, is_in_pips=True)
+        self.account.take_profit(trade_id, 40, is_in_pips=True)
+        self.account.stop_loss(trade_id, -40, is_in_pips=True)
 
         self.account.close_trade(trade_id, 0.005)
         self.assertTrue(len(self.account.list_open_trade()) == 1 + trade_count)
         trade_id = self.account.open_trade_ids()[0]
         self.account.close_trade(trade_id)
+        sleep(5)
         self.assertEqual(len(self.account.list_open_trade()), trade_count)
         self.assertEqual(len(self.account.open_positions), position_count)
 
@@ -50,9 +53,12 @@ class FXCMTestAccount(unittest.TestCase):
         self.assertTrue(order2)
         self.assertEqual(len(self.account.open_order_ids()), 2 + order_count)
 
-        self.account.take_profit(self, order1.get_orderId(), 40, is_in_pips=True)
-        self.account.stop_loss(self, order2.get_orderId(), -40, is_in_pips=True)
+        self.account.take_profit(order1.get_orderId(), 40, is_in_pips=True)
+        self.account.stop_loss(order2.get_orderId(), -40, is_in_pips=True)
 
         self.account.cancel_order(order1.get_orderId())
         self.account.cancel_order(order2.get_orderId())
         self.assertEqual(len(self.account.open_order_ids()), order_count)
+
+        self.account.close_all_position()
+        self.assertEqual(len(self.account.open_order_ids()), 0)
