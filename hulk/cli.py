8c6a8272b29a59fceee936f4545286bad3d6296c
Lorne <dev@luotao.net>
2019-11-13 13:13:40 +1100

    refactor update requirements

--- hulk/cli.py
+++ hulk/cli.py
@@ -1,18 +1,18 @@
-# -*- coding: utf-8 -*-
-
-"""Console script for hulk."""
-import sys
-import click
-
-
-@click.command()
-def main(args=None):
-    """Console script for hulk."""
-    click.echo("Replace this message by putting your code into "
-               "hulk.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
-
-
-if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+# # -*- coding: utf-8 -*-
+#
+# """Console script for hulk."""
+# import sys
+# import click
+#
+#
+# @click.command()
+# def main(args=None):
+#     """Console script for hulk."""
+#     click.echo("Replace this message by putting your code into "
+#                "hulk.cli.main")
+#     click.echo("See click documentation at https://click.palletsprojects.com/")
+#     return 0
+#
+#
+# if __name__ == "__main__":
+#     sys.exit(main())  # pragma: no cover
